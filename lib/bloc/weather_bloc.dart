fcc24a247a4f9e5206b743eb2cadfb4d9050004e
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2020-03-01 19:35:28 +0100

    Added bloc logic

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -3,11 +3,16 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:rain/data/models/weather.dart';
+import 'package:rain/data/repositories/weather_repo.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final WeatherRepository repository;
+
+  WeatherBloc(this.repository);
+
   @override
   WeatherState get initialState => WeatherInitial();
 
@@ -15,6 +20,15 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield WeatherLoading();
+
+    if (event is GetWeather) {
+      try {
+        final Weather weather = await repository.fetchWeather();
+        yield WeatherLoaded(weather);
+      } catch (e) {
+        yield WeatherError(e.toString());
+      }
+    }
   }
 }
